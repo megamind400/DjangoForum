2d7955e0f2e844b6b3ca4d6ec253bbeb456b8bba
megamind400 <megamind741074107410@gmail.com>
2022-02-02 18:07:49 +0330

    commenting added

--- base/views.py
+++ base/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
 from .forms import RoomForm
-from .models import Room,Topic
+from .models import Room,Topic, Message
 
 
 
@@ -34,8 +34,16 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
 
-    context = {'room': room}
+    if request.method == 'POST':
+        message = Message.objects.create(
+                user= request.user,
+                room= room,
+                body= request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html',context )    
 
 @login_required(login_url='loginreg') #if the user is not logged in redirect to 'loginreg'
